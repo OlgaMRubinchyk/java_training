04d6700b41144661a34524170534764be136ca5a
OlgaMRubinchyk <109346805+OlgaMRubinchyk@users.noreply.github.com>
2023-09-17 19:28:44 +0300

    committing task 05

---
+++ src/lesson_05/hometask_05/Task06.java
@@ -0,0 +1,19 @@
+package lesson_05.hometask_05;
+/*Write a program where the user must enter a string that contains letters and
+        numbers.
+        The program should output all the numbers from the string, as well as the sum
+        of the numbers.
+        Example:
+        Input:
+        test12ing45how-12this65works
+        Output:
+        12
+        45
+        -12
+        65
+        The sum of all numbers is: 110*/
+public class Task06 {
+    public static void main(String[] args) {
+
+    }
+}
