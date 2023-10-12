b4edf1e02ac44135aabc0d0ca611cde3064ef8c7
OlgaMRubinchyk <109346805+OlgaMRubinchyk@users.noreply.github.com>
2023-10-12 11:47:26 +0300

    Task02 is ready, 1 and 3 in progress

---
+++ src/lesson_13/Task01.java
@@ -0,0 +1,7 @@
+package lesson_13;
+
+public class Task01 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/lesson_13/Task03.java
@@ -0,0 +1,16 @@
+package lesson_13;
+
+import java.util.Scanner;
+
+public class Task03 {
+    public static void main(String[] args) {
+        // user enters text
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter user's text: ");
+        String userText = scanner.nextLine();
+        userText.toLowerCase();
+        //userText = "cbaz";
+
+
+    }
+}
